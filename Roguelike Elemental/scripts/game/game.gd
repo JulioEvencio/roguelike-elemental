014fe15f1d2e9694b54928e3614211927f1a9ae0
JulioEvencio <julioigreja.dev@gmail.com>
2024-08-10 20:27:37 -0300

    added: world tiles and update configs

--- Roguelike Elemental/scripts/game/game.gd
+++ Roguelike Elemental/scripts/game/game.gd
@@ -1,19 +1,24 @@
 class_name Game extends Node
 
-@onready var _fps_label: Label = get_node("Debug/ColorRect/FPS")
 @onready var _scenario: Node = get_node("Scenario")
 
 func _ready() -> void:
-	_update_scenario(preload("res://scenes/scenarios/world.tscn"))
+	_start_game()
 
 func _physics_process(_delta: float) -> void:
-	_fps_label.text = "FPS: " + str(Engine.get_frames_per_second())
-	
 	if Input.is_action_just_pressed("escape"):
 		get_tree().quit()
 
-func _update_scenario(scenario: PackedScene) -> void:
-	for child in _scenario.get_children():
+func _start_game() -> void:
+	var scenario_scene: PackedScene = load("res://scenes/scenarios/world.tscn")
+	var player_scene: PackedScene = load("res://scenes/actors/player/player_fire.tscn")
+	
+	var scenario: Scenario = scenario_scene.instantiate()
+	var player: Player = player_scene.instantiate()
+	
+	scenario.add_player(player)
+	
+	for child: Scenario in _scenario.get_children():
 		child.queue_free()
 	
-	_scenario.add_child(scenario.instantiate())
+	_scenario.add_child(scenario)
---
+++ Roguelike Elemental/scripts/scenarios/world.gd
@@ -0,0 +1,29 @@
+class_name Scenario extends Node2D
+
+var _player: Player
+
+func _ready() -> void:
+	_setup_camera()
+	_setup_player()
+
+func _setup_camera() -> void:
+	var camera: Camera2D = get_node("Camera2D")
+	var camera_limit_left: Marker2D = get_node("WorldLimits/CameraLimitLeft")
+	var camera_limit_right: Marker2D = get_node("WorldLimits/CameraLimitRight")
+	var camera_limit_top: Marker2D = get_node("WorldLimits/CameraLimitTop")
+	var camera_limit_bottom: Marker2D = get_node("WorldLimits/CameraLimitBottom")
+	
+	camera.limit_left = int(camera_limit_left.position.x)
+	camera.limit_right = int(camera_limit_right.position.x)
+	camera.limit_top = int(camera_limit_top.position.y)
+	camera.limit_bottom = int(camera_limit_bottom.position.y)
+
+func _setup_player() -> void:
+	var player_spwan: Marker2D = get_node("PlayerSpwan")
+	
+	_player.position = player_spwan.position
+	
+	add_child(_player)
+
+func add_player(player: Player) -> void:
+	_player = player
