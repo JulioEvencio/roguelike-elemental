36cc420bad5fc091d12cba8f3d5ba28be384a7f0
JulioEvencio <julioigreja.dev@gmail.com>
2024-08-12 21:00:18 -0300

    added: transition

--- Roguelike Elemental/scripts/screens/main_menu.gd
+++ Roguelike Elemental/scripts/screens/main_menu.gd
@@ -1,13 +1,25 @@
 class_name MainMenu extends Control
 
-func _on_play_pressed() -> void:
+func _play() -> void:
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
 
-func _on_settings_pressed() -> void:
+func _settings() -> void:
 	pass
+
+func _credits() -> void:
+	pass
+
+func _exit() -> void:
+	get_tree().quit()
+
+func _on_play_pressed() -> void:
+	Transition.start(func(): _play())
+
+func _on_settings_pressed() -> void:
+	Transition.start(func(): _settings())
 
 func _on_credits_pressed() -> void:
-	pass
+	Transition.start(func(): _credits())
 
 func _on_exit_pressed() -> void:
-	get_tree().quit()
+	Transition.start(func(): _exit())
---
+++ Roguelike Elemental/scripts/transitions/transition.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+var _lambda: Callable
+
+@onready var _animation_player: AnimationPlayer = get_node("AnimationPlayer")
+
+func _ready() -> void:
+	_animation_player.play("fade_out")
+
+func start(callable: Callable) -> void:
+	_lambda = callable
+	_animation_player.play("fade_in")
+
+func _call_lambda() -> void:
+	_lambda.call()
+	_animation_player.play("fade_out")
+
+func _on_animation_player_animation_finished(anim_name: String) -> void:
+	match anim_name:
+		"fade_in":
+			call_deferred("_call_lambda")
