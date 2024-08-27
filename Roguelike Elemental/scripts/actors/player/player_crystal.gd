423486365503c89a9b79b4e0f10c496f16aeec5c
JulioEvencio <julioigreja.dev@gmail.com>
2024-08-26 23:01:05 -0300

    update: logic attack player crystal

--- Roguelike Elemental/scripts/actors/player/player_crystal.gd
+++ Roguelike Elemental/scripts/actors/player/player_crystal.gd
@@ -4,10 +4,10 @@ func _ready() -> void:
 	status.passive_critical = true
 
 func _logic_damage_attack_02() -> int:
-	return 2
+	return 2 + int(float(status.damage) / 10)
 
 func _logic_damage_attack_03() -> int:
-	return 10
+	return 3 + int(float(status.damage) / 10)
 
 func _logic_damage_attack_special() -> int:
-	return 20
+	return 10 + int(float(status.damage) / 50)
